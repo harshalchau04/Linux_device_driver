d6ebb078504b513b6aadb2b73112e5da54a7217b
HARSHAL CHADHARI <harshalchau04@gmail.com>
2019-03-23 22:31:32 +0530

    Starting of Driver

--- GPIO/first_interface_exit.c
+++
@@ -1,8 +0,0 @@
-#include<linux/init.h>
-#include<linux/module.h>
-
-void first_interface_exit(void)
-{
-	printk(KERN_ALERT "Inside the %s function",__FUNCTION__);
-}
-module_exit(first_interface_exit);
---
+++ GPIO/raspi_gpio_driver.c
@@ -0,0 +1,23 @@
+#include<linux/module.h>
+#include<iinux/kernel.h>
+
+static int __init
+raspi_gpio_init(void)
+{
+	printf(KERN_ALERT "INSIDE %s YES I CAN WRITE DRIVER",__FUNCTION__);
+	return 0;
+}
+
+static void __exit
+raspi_gpio_exit(void)
+{
+	 printf(KERN_ALERT "INSIDE %s YES I DONE IT!!!!!!",__FUNCTION__);
+	 return 0;
+}
+
+module_init(raspi_gpio_init);
+module_exit(raspi_gpio_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("HARSHAL CHAUDHARI <harshalchau04@gmail.com>");
+MODULE_DESCRIPTION("GPIO device driver for Raspberry Pi 3");
